44e7ba59ca99aa18be9c2ff263804911772e0653
act18 <act18@sfu.ca>
2022-12-05 16:04:47 -0800

    mulithreadtest shows multithreadedness

--- TestMultiThreadServer.py
+++ TestMultiThreadServer.py
@@ -7,13 +7,13 @@
 
 from multiprocessing import Process, Manager
 import multiprocessing as mp
-import threading
+import random
 import time
 from MultiThreadServer import start_server
 from socket import *
 import socket
 
-MAX_RUNTIME = 10 # second
+
 SERVER_NAME = socket.gethostbyname(socket.gethostname())
 SERVER_PORT = 12002
 
@@ -34,100 +34,123 @@ def client_connection(request):
 
 
 # Description: Send a request for test.html. File should be found (200)
-def server_test_1(testResults):
-    print("Executing test 1...")
+def server_test_1(results, clientNumber):
+    print(f"Executing test 1 for client {clientNumber+1}...")
     request = ("GET /test.html HTTP/1.1\r\n" + 
                 "Host: " + str(SERVER_NAME) + ":" + str(SERVER_PORT) + "\r\n")
     serverResponse = client_connection(request)
     # validate response
     expectedResult = "200"
     response = serverResponse.decode().split(" ")
-    testResults[0] = "PASS" if response[1] == expectedResult else "FAIL"
+    status = "PASS" if response[1] == expectedResult else "FAIL"
+    results[clientNumber][0] = {"status": status, "time": time.time()}
 
-def server_test_2(testResults):
-    print("Executing test 2...")
+def server_test_2(results, clientNumber):
+    print(f"Executing test 2 for client {clientNumber+1}...")
     request = ("GET /test1.html HTTP/1.1\r\n" + 
                 "Host: " + str(SERVER_NAME) + ":" + str(SERVER_PORT) + "\r\n")
     serverResponse = client_connection(request)
     # validate response
     expectedResult = "200"
     response = serverResponse.decode().split(" ")
-    testResults[1] = "PASS" if response[1] == expectedResult else "FAIL"
+    status = "PASS" if response[1] == expectedResult else "FAIL"
+    results[clientNumber][1] = {"status": status, "time": time.time()}
 
-def server_test_3(testResults):
-    print("Executing test 3...")
+def server_test_3(results, clientNumber):
+    print(f"Executing test 3 for client {clientNumber+1}...")
     request = ("GET /test2.html HTTP/1.1\r\n" + 
                 "Host: " + str(SERVER_NAME) + ":" + str(SERVER_PORT) + "\r\n")
     serverResponse = client_connection(request)
     # validate response
     expectedResult = "200"
     response = serverResponse.decode().split(" ")
-    testResults[2] = "PASS" if response[1] == expectedResult else "FAIL"
-
-def server_test_4(testResults):
-    print("Executing test 4...")
+    status = "PASS" if response[1] == expectedResult else "FAIL"
+    results[clientNumber][2] = {"status": status, "time": time.time()}
+    
+def server_test_4(results, clientNumber):
+    print(f"Executing test 4 for client {clientNumber+1}...")
     request = ("GET /test3.html HTTP/1.1\r\n" + 
                 "Host: " + str(SERVER_NAME) + ":" + str(SERVER_PORT) + "\r\n")
     serverResponse = client_connection(request)
     # validate response
     expectedResult = "200"
     response = serverResponse.decode().split(" ")
-    testResults[3] = "PASS" if response[1] == expectedResult else "FAIL"
+    status = "PASS" if response[1] == expectedResult else "FAIL"
+    results[clientNumber][3] = {"status": status, "time": time.time()}
 
-def server_test_5(testResults):
-    print("Executing test 5...")
+def server_test_5(results, clientNumber):
+    print(f"Executing test 5 for client {clientNumber+1}...")
     request = ("GET /test4.html HTTP/1.1\r\n" + 
                 "Host: " + str(SERVER_NAME) + ":" + str(SERVER_PORT) + "\r\n")
     serverResponse = client_connection(request)
     # validate response
     expectedResult = "200"
     response = serverResponse.decode().split(" ")
-    testResults[4] = "PASS" if response[1] == expectedResult else "FAIL"
-
-def server_test_6(testResults):
-    print("Executing test 6...")
+    status = "PASS" if response[1] == expectedResult else "FAIL"
+    results[clientNumber][4] = {"status": status, "time": time.time()}
+    
+def server_test_6(results, clientNumber):
+    print(f"Executing test 6 for client {clientNumber+1}...")
     request = ("GET /test5.html HTTP/1.1\r\n" + 
                 "Host: " + str(SERVER_NAME) + ":" + str(SERVER_PORT) + "\r\n")
     serverResponse = client_connection(request)
     # validate response
     expectedResult = "200"
     response = serverResponse.decode().split(" ")
-    testResults[5] = "PASS" if response[1] == expectedResult else "FAIL"
+    status = "PASS" if response[1] == expectedResult else "FAIL"
+    results[clientNumber][5] = {"status": status, "time": time.time()}
 
 # Description: Send a request for test.htm. Bad request because unknown file extension (400)
-def server_test_7(testResults):
-    print("Executing test 7...")
+def server_test_7(results, clientNumber):
+    print(f"Executing test 7 for client {clientNumber+1}...")
     request = ("GET /test.htm HTTP/1.1\r\n" + 
                 "Host: " + str(SERVER_NAME) + ":" + str(SERVER_PORT) + "\r\n")
     serverResponse = client_connection(request)
     # validate response
     expectedResult = "400"
     response = serverResponse.decode().split(" ")
-    testResults[6] = "PASS" if response[1] == expectedResult else "FAIL"    
+    status = "PASS" if response[1] == expectedResult else "FAIL"
+    results[clientNumber][6] = {"status": status, "time": time.time()}  
 
 # Description: Send a request for notFound.html. File should not be found (404)
-def server_test_8(testResults):
-    print("Executing test 8...")
+def server_test_8(results, clientNumber):
+    print(f"Executing test 8 for client {clientNumber+1}...")
     request = ("GET /notFound.html HTTP/1.1\r\n" + 
                 "Host: " + str(SERVER_NAME) + ":" + str(SERVER_PORT) + "\r\n")
     serverResponse = client_connection(request)
     # validate response
     expectedResult = "404"
     response = serverResponse.decode().split(" ")
-    testResults[7] = "PASS" if response[1] == expectedResult else "FAIL"
+    status = "PASS" if response[1] == expectedResult else "FAIL"
+    results[clientNumber][7] = {"status": status, "time": time.time()}
 
 
 # Description: Execute all test cases in a sequential order since the server
 #               is singly-threaded
-def server_tests(testResults):
-    server_test_1(testResults)
-    server_test_2(testResults)
-    server_test_3(testResults)
-    server_test_4(testResults)
-    server_test_5(testResults)
-    server_test_6(testResults)
-    server_test_7(testResults)
-    server_test_8(testResults)
+def server_tests(results, clientNumber, numberOfClients):
+    tests = [1, 2, 3, 4, 5, 6, 7, 8]
+    while len(tests) != 0:
+        time.sleep(random.randint(0, numberOfClients))
+        testCase = random.choice(tests)
+        if testCase == 1:
+            server_test_1(results, clientNumber)
+        elif testCase == 2:
+            server_test_2(results, clientNumber)
+        elif testCase == 3:
+            server_test_3(results, clientNumber)
+        elif testCase == 4:
+            server_test_4(results, clientNumber)
+        elif testCase == 5:
+            server_test_5(results, clientNumber)
+        elif testCase == 6:
+            server_test_6(results, clientNumber)
+        elif testCase == 7:
+            server_test_7(results, clientNumber)
+        elif testCase == 8:
+            server_test_8(results, clientNumber)
+        tests.remove(testCase)
+
+
 
 # Description: Run the server
 def run_server():
@@ -136,35 +159,46 @@ def run_server():
 
 
 if __name__ == "__main__":
+
+    expectedCode = [200, 200, 200, 200, 200, 200, 400, 404] # expected codes for test cases
+    numberOfClients = 3;
+    max_runtime = len(expectedCode) * numberOfClients * numberOfClients
+
     with Manager() as manager:
-        testResults = manager.dict()   # dictionary of test case results
+
+        results = []
+        for i in range(numberOfClients):
+            testResults = manager.dict()   # dictionary of test case results
+            results.append(testResults)            
 
         print("Executing tests...")
         run = Process(target=run_server) # process to run the server
-        test = Process(target=server_tests, args=(testResults,)) # process to execute test cases
-        
         run.start()
-        test.start()
+
+        p = []
+        for clientNumber in range(numberOfClients):
+            p.append(Process(target=server_tests, args=(results, clientNumber, numberOfClients, ))) # process to execute test cases
         
-        time.sleep(MAX_RUNTIME)
+        for process in p:
+            process.start()
         
-        if (test.is_alive()):
-            test.terminate()
+        time.sleep(max_runtime)
+        
+        for process in p:
+            if process.is_alive():
+                process.terminate()
+            process.join()
 
         run.terminate()
-        
-        test.join()
         run.join()
 
-        print("\n#########################################")
-        print("#              TEST RESULTS             #")
-        print("#                                       #")
-        print("#\tTest 1 (200):\t\t" + testResults[0] + "\t#")
-        print("#\tTest 2 (200):\t\t" + testResults[1] + "\t#")
-        print("#\tTest 3 (200):\t\t" + testResults[2] + "\t#")
-        print("#\tTest 4 (200):\t\t" + testResults[3] + "\t#")
-        print("#\tTest 5 (200):\t\t" + testResults[4] + "\t#")
-        print("#\tTest 6 (200):\t\t" + testResults[5] + "\t#")
-        print("#\tTest 7 (400):\t\t" + testResults[6] + "\t#")
-        print("#\tTest 8 (404):\t\t" + testResults[7] + "\t#")
-        print("#########################################\n")
+        print("\n#################################################################################")
+        print("#                                  TEST RESULTS                                 #")
+        print("#                                                                               #")
+        for index in range(len(expectedCode)):
+            for testResult in range(numberOfClients):
+                s = results[testResult][index]['status']
+                t = results[testResult][index]['time']
+                print(f"#\tTest {index+1} [Client-{testResult+1}] ({expectedCode[index]}):\t {s}\t[{time.ctime(t)}]\t#")
+            print("#                                                                               #")
+        print("#################################################################################\n")
